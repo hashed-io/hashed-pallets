7e022eeb3144acab3b695cd86e0ae91176a6f535
didiermis <didier.mis.may@gmail.com>
2023-07-20 17:20:03 -0600

    Introduced key types and struct required to support the addition of records in types.rs. The new types include `CID`, `Description`, `RecordId`, `ProjectId`, `CreationDate`, and `UpdateDate`. Created `RecordData` struct to represent a record, and a `Table` enum to classify records

---
+++ pallets/fund-admin-records/src/types.rs
@@ -0,0 +1,27 @@
+use super::*;
+use frame_support::pallet_prelude::*;
+use frame_support::sp_io::hashing::blake2_256;
+use sp_runtime::sp_std::vec::Vec;
+
+pub type CID = BoundedVec<u8, ConstU32<100>>;
+pub type Description = BoundedVec<u8, ConstU32<400>>;
+pub type RecordId = [u8; 32];
+pub type ProjectId = [u8; 36];
+pub type CreationDate = u64;
+pub type UpdateDate = u64;
+
+#[derive(CloneNoBound, Encode, Decode, RuntimeDebugNoBound, Default, TypeInfo, MaxEncodedLen)]
+pub struct RecordData {
+  pub cid: CID,
+  pub description: Description,
+  pub creation_date: CreationDate,
+  pub updated_date: Option<UpdateDate>,
+}
+
+#[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebugNoBound, MaxEncodedLen, TypeInfo, Copy)]
+pub enum Table {
+  Drawdown,
+  RecoveryDrawdown,
+  Revenue,
+  RecoveryRevenue,
+}
