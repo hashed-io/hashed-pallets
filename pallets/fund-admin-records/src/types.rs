6dc668e538c0b0143abc4ebd6599e90893b187f9
didiermis <didier.mis.may@gmail.com>
2023-07-24 19:43:12 -0600

    Implemented SR25519-based app-specific crypto type named 'TestAuthId' within the Fund Admin Records pallet using Substrate's `AppCrypto` trait. This crypto type is intended for use with off-chain signatures verification and is currently assigned to the `KEY_TYPE` 'funadmin'.

--- pallets/fund-admin-records/src/types.rs
+++ pallets/fund-admin-records/src/types.rs
@@ -2,6 +2,37 @@ use super::*;
 use frame_support::pallet_prelude::*;
 use frame_support::sp_io::hashing::blake2_256;
 use sp_runtime::sp_std::vec::Vec;
+use sp_core::crypto::KeyTypeId;
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"funadmin");
+
+pub mod crypto {
+	use super::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519},
+		traits::Verify, MultiSignature, MultiSigner
+	};
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+
+	// implemented for runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+
+	// implemented for mock runtime in test
+	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
+		for TestAuthId
+	{
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+}
 
 pub type CID = BoundedVec<u8, ConstU32<100>>;
 pub type Description = BoundedVec<u8, ConstU32<400>>;
@@ -25,3 +56,4 @@ pub enum Table {
   Revenue,
   RecoveryRevenue,
 }
+
