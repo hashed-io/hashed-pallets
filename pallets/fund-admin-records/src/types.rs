c6c7e5dcc06395af06a9969bd0f4fab36e6b25ad
didiermis <didier.mis.may@gmail.com>
2023-07-26 23:10:57 -0600

    Simplified the structure `RecordData` by replacing `cid` and `description` with `hashed_info`, which unifies data storage and reduces complexity. The `ProjectId` now has a length of 60 bytes instead of the previous 32. Removed extraneous implementations and data structures that were previously used for signed payload handling, as the pallet now only works with signed transactions.

--- pallets/fund-admin-records/src/types.rs
+++ pallets/fund-admin-records/src/types.rs
@@ -1,87 +1,17 @@
 // use super::*;
 use frame_support::pallet_prelude::*;
-use sp_runtime::sp_std::vec::Vec;
-use sp_core::crypto::KeyTypeId;
-use frame_system::offchain::{SigningTypes, SignedPayload};
 
-pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"fund");
-pub const UNSIGNED_TXS_PRIORITY: u64 = 100;
-
-pub mod crypto {
-	use super::KEY_TYPE;
-	use sp_core::sr25519::Signature as Sr25519Signature;
-	use sp_runtime::{
-		app_crypto::{app_crypto, sr25519},
-		traits::Verify, MultiSignature, MultiSigner
-	};
-	app_crypto!(sr25519, KEY_TYPE);
-
-	pub struct TestAuthId;
-
-	// implemented for runtime
-	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
-		type RuntimeAppPublic = Public;
-		type GenericSignature = sp_core::sr25519::Signature;
-		type GenericPublic = sp_core::sr25519::Public;
-	}
-
-	// implemented for mock runtime in test
-	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
-		for TestAuthId
-	{
-		type RuntimeAppPublic = Public;
-		type GenericSignature = sp_core::sr25519::Signature;
-		type GenericPublic = sp_core::sr25519::Public;
-	}
-}
-pub type CID = BoundedVec<u8, ConstU32<100>>;
-pub type Description = BoundedVec<u8, ConstU32<400>>;
+pub type HashedInfo = [u8; 60];
 pub type Id = [u8; 32];
-pub type ProjectId = [u8; 32];
+pub type ProjectId = [u8; 60];
 pub type CreationDate = u64;
 
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-#[codec(mel_bound())]
-pub struct RecordsPayload<Public> {
-	pub records_payload: Vec<SingleRecordPayload>,
-	pub public: Public,
-}
-
-#[derive(Encode, Decode, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-#[codec(mel_bound())]
-pub struct SingleRecordPayload {
-	pub project_id: ProjectId,
-	pub cid: CID,
-	pub description: Description,
-	pub table: Table,
-	pub record_type: RecordType,
-}
-
-impl Clone for SingleRecordPayload {
-	fn clone(&self) -> Self {
-		Self {
-			project_id: self.project_id.clone(),
-			cid: self.cid.clone(),
-			description: self.description.clone(),
-			table: self.table.clone(),
-			record_type: self.record_type.clone(),
-		}
-	}
-}
-
-impl<S: SigningTypes > SignedPayload<S> for RecordsPayload<S::Public> {
-	fn public(&self) -> S::Public {
-		self.public.clone()
-	}
-}
-
-#[derive(Encode, Decode, RuntimeDebugNoBound, Default, TypeInfo, MaxEncodedLen,)]
+#[derive(Encode, Decode, RuntimeDebugNoBound, TypeInfo, MaxEncodedLen,)]
 #[scale_info(skip_type_params(MaxLen))]
 #[codec(mel_bound())]
 pub struct RecordData {
 	pub project_id: ProjectId,
-	pub cid: CID,
-	pub description: Description,
+	pub hashed_info: HashedInfo,
 	pub table: Table,
 	pub record_type: RecordType,
 	pub creation_date: CreationDate,
@@ -95,12 +25,6 @@ pub enum Table {
   RecoveryRevenue,
 }
 
-impl Default for Table {
-	fn default() -> Self {
-		Table::Drawdown
-	}
-}
-
 #[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebugNoBound, MaxEncodedLen, TypeInfo, Copy)]
 pub enum RecordType {
 	Creation,
@@ -109,10 +33,4 @@ pub enum RecordType {
 	Reject,
 	Recovery,
 	Reset,
-}
-
-impl Default for RecordType {
-	fn default() -> Self {
-		RecordType::Creation
-	}
 }
