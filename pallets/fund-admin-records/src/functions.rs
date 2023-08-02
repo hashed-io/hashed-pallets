8b58b146617f61d5666e4e0490636842515290eb
didiermis <didier.mis.may@gmail.com>
2023-08-02 14:29:57 -0600

    ntroduced new validations in the `do_add_record` function in the Fund Admin Records pallet. Ensured that `project_id` and `hashed_info` are non-empty and fall within their set maximum lengths, else they will throw errors. The validated data are saved in bound vectors which are then used to compute the unique record id and inserted into the record_data object, ensuring the optimized storage and retrieval of record data.

--- pallets/fund-admin-records/src/functions.rs
+++ pallets/fund-admin-records/src/functions.rs
@@ -13,17 +13,27 @@ impl<T: Config> Pallet<T> {
     table: Table,
     record_type: RecordType,
   ) -> DispatchResult{
+    // Validations
+    ensure!(!project_id.is_empty(), Error::<T>::ProjectIdIsEmpty);
+    ensure!(!hashed_info.is_empty(), Error::<T>::HashedInfoIsEmpty);
+
+    let project_id_validated = BoundedVec::<u8, ConstU32<50>>::try_from(project_id.clone())
+      .map_err(|_| Error::<T>::ProjectIdExceededMaxLength)?;
+
+    let hashed_info_validated = BoundedVec::<u8, ConstU32<400>>::try_from(hashed_info.clone())
+      .map_err(|_| Error::<T>::HashedInfoExceededMaxLength)?;
+
     // Get timestamp
     let creation_date: CreationDate = Self::get_timestamp_in_milliseconds().ok_or(Error::<T>::TimestampError)?;
 
-    let record_id: Id = (project_id, creation_date).using_encoded(blake2_256);
+    let record_id: Id = (project_id.clone(), creation_date).using_encoded(blake2_256);
 
     // Ensure the generated id is unique
-    ensure!(!Records::<T>::contains_key((project_id, table), record_id), Error::<T>::IdAlreadyExists);
+    ensure!(!Records::<T>::contains_key((project_id.clone(), table), record_id), Error::<T>::IdAlreadyExists);
 
     let record_data = RecordData {
-      project_id,
-      hashed_info,
+      project_id: project_id_validated,
+      hashed_info: hashed_info_validated,
       table,
       record_type,
       creation_date,
@@ -31,7 +41,7 @@ impl<T: Config> Pallet<T> {
 
     // Insert the record into the storage
     <Records<T>>::insert(
-      (project_id, table),
+      (project_id.clone(), table),
       &record_id,
       record_data
     );
