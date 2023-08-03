6f9765d80325a2376bf1a0731b5e093452192da6
didiermis <didier.mis.may@gmail.com>
2023-08-03 11:39:53 -0600

    Significantly restructured the `do_add_record` function in the Fund Admin Records pallet. This function now takes in a collection of records and performs validations on each record in the collection. The resulting `RecordData` also includes `table_type` instead of `Table`, reflecting the changes in the record data structure. The generate `RecordId` is ensured to be unique, and if true, the data is saved in the storage and an associated event is emitted.

--- pallets/fund-admin-records/src/functions.rs
+++ pallets/fund-admin-records/src/functions.rs
@@ -8,46 +8,44 @@ use crate::types::*;
 impl<T: Config> Pallet<T> {
   /*---- Offchain extrinsics ----*/
   pub fn do_add_record(
-    project_id: ProjectId,
-    hashed_info: HashedInfo,
-    table: Table,
-    record_type: RecordType,
+    records: RecordCollection<T>,
   ) -> DispatchResult{
-    // Validations
-    ensure!(!project_id.is_empty(), Error::<T>::ProjectIdIsEmpty);
-    ensure!(!hashed_info.is_empty(), Error::<T>::HashedInfoIsEmpty);
+    for record in records.iter().cloned() {
+      // Validations
+      ensure!(!record.0.is_empty(), Error::<T>::ProjectIdIsEmpty);
+      ensure!(!record.1.is_empty(), Error::<T>::HashedInfoIsEmpty);
 
-    let project_id_validated = BoundedVec::<u8, ConstU32<50>>::try_from(project_id.clone())
-      .map_err(|_| Error::<T>::ProjectIdExceededMaxLength)?;
+      let project_id_validated = BoundedVec::<u8, ConstU32<50>>::try_from(record.0.clone())
+        .map_err(|_| Error::<T>::ProjectIdExceededMaxLength)?;
 
-    let hashed_info_validated = BoundedVec::<u8, ConstU32<400>>::try_from(hashed_info.clone())
-      .map_err(|_| Error::<T>::HashedInfoExceededMaxLength)?;
+      let hashed_info_validated = BoundedVec::<u8, ConstU32<400>>::try_from(record.1.clone())
+        .map_err(|_| Error::<T>::HashedInfoExceededMaxLength)?;
 
-    // Get timestamp
-    let creation_date: CreationDate = Self::get_timestamp_in_milliseconds().ok_or(Error::<T>::TimestampError)?;
+      // Get timestamp
+      let creation_date: CreationDate = Self::get_timestamp_in_milliseconds().ok_or(Error::<T>::TimestampError)?;
 
-    let record_id: Id = (project_id.clone(), creation_date).using_encoded(blake2_256);
+      let record_id: Id = (record.0.clone(), creation_date).using_encoded(blake2_256);
 
-    // Ensure the generated id is unique
-    ensure!(!Records::<T>::contains_key((project_id.clone(), table), record_id), Error::<T>::IdAlreadyExists);
-
-    let record_data = RecordData {
-      project_id: project_id_validated,
-      hashed_info: hashed_info_validated,
-      table,
-      record_type,
-      creation_date,
-    };
-
-    // Insert the record into the storage
-    <Records<T>>::insert(
-      (project_id.clone(), table),
-      &record_id,
-      record_data
-    );
-
-    Self::deposit_event(Event::RecordAdded(project_id, table, record_type, record_id));
-
+      // Ensure the generated id is unique
+      ensure!(!Records::<T>::contains_key((record.0.clone(), record.2), record_id), Error::<T>::IdAlreadyExists);
+      
+      let record_data = RecordData {
+        project_id: project_id_validated,
+        hashed_info: hashed_info_validated,
+        table_type: record.2,
+        record_type: record.3,
+        creation_date,
+      };
+  
+      // Insert the record into the storage
+      <Records<T>>::insert(
+        (record.0.clone(), record.2),
+        &record_id,
+        record_data
+      );
+  
+      Self::deposit_event(Event::RecordAdded(record.0, record.2, record.3, record_id));
+    }
     Ok(())
   }
 
