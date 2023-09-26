48a7d51a09a94b7395e72d90740292e682ae6fa4
tlacloc <desneruda@gmail.com>
2023-09-26 12:35:11 -0600

    update format with rustfmt

--- pallets/mapped-assets/src/mock.rs
+++ pallets/mapped-assets/src/mock.rs
@@ -22,30 +22,30 @@ use crate as pallet_assets;
 
 use codec::Encode;
 use frame_support::{
-  construct_runtime, parameter_types,
-  traits::{AsEnsureOriginWithArg, ConstU32, ConstU64, GenesisBuild},
+	construct_runtime, parameter_types,
+	traits::{AsEnsureOriginWithArg, ConstU32, ConstU64, GenesisBuild},
 };
+use frame_system::EnsureRoot;
 use sp_core::H256;
 use sp_io::storage;
 use sp_runtime::{
-  testing::Header,
-  traits::{BlakeTwo256, IdentityLookup},
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
 };
-use frame_system::EnsureRoot;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 construct_runtime!(
   pub enum Test where
-    Block = Block,
-    NodeBlock = Block,
-    UncheckedExtrinsic = UncheckedExtrinsic,
+	Block = Block,
+	NodeBlock = Block,
+	UncheckedExtrinsic = UncheckedExtrinsic,
   {
-    System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-    Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-    Assets: pallet_assets::{Pallet, Call, Storage, Event<T>},
-    RBAC: pallet_rbac::{Pallet, Call, Storage, Event<T>},
+	System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+	Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+	Assets: pallet_assets::{Pallet, Call, Storage, Event<T>},
+	RBAC: pallet_rbac::{Pallet, Call, Storage, Event<T>},
   }
 );
 
@@ -53,30 +53,30 @@ type AccountId = u64;
 type AssetId = u32;
 
 impl frame_system::Config for Test {
-  type BaseCallFilter = frame_support::traits::Everything;
-  type BlockWeights = ();
-  type BlockLength = ();
-  type RuntimeOrigin = RuntimeOrigin;
-  type RuntimeCall = RuntimeCall;
-  type Index = u64;
-  type BlockNumber = u64;
-  type Hash = H256;
-  type Hashing = BlakeTwo256;
-  type AccountId = u64;
-  type Lookup = IdentityLookup<Self::AccountId>;
-  type Header = Header;
-  type RuntimeEvent = RuntimeEvent;
-  type BlockHashCount = ConstU64<250>;
-  type DbWeight = ();
-  type Version = ();
-  type PalletInfo = PalletInfo;
-  type AccountData = pallet_balances::AccountData<u64>;
-  type OnNewAccount = ();
-  type OnKilledAccount = ();
-  type SystemWeightInfo = ();
-  type SS58Prefix = ();
-  type OnSetCode = ();
-  type MaxConsumers = ConstU32<2>;
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<u64>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<2>;
 }
 
 parameter_types! {
@@ -89,38 +89,38 @@ parameter_types! {
   pub const MaxUsersPerRole: u32 = 2;
 }
 impl pallet_rbac::Config for Test {
-  type RuntimeEvent = RuntimeEvent;
-  type RemoveOrigin = EnsureRoot<Self::AccountId>;
-  type MaxScopesPerPallet = MaxScopesPerPallet;
-  type MaxRolesPerPallet = MaxRolesPerPallet;
-  type RoleMaxLen = RoleMaxLen;
-  type PermissionMaxLen = PermissionMaxLen;
-  type MaxPermissionsPerRole = MaxPermissionsPerRole;
-  type MaxRolesPerUser = MaxRolesPerUser;
-  type MaxUsersPerRole = MaxUsersPerRole;
+	type RuntimeEvent = RuntimeEvent;
+	type RemoveOrigin = EnsureRoot<Self::AccountId>;
+	type MaxScopesPerPallet = MaxScopesPerPallet;
+	type MaxRolesPerPallet = MaxRolesPerPallet;
+	type RoleMaxLen = RoleMaxLen;
+	type PermissionMaxLen = PermissionMaxLen;
+	type MaxPermissionsPerRole = MaxPermissionsPerRole;
+	type MaxRolesPerUser = MaxRolesPerUser;
+	type MaxUsersPerRole = MaxUsersPerRole;
 }
 
 impl pallet_balances::Config for Test {
-  type Balance = u64;
-  type DustRemoval = ();
-  type RuntimeEvent = RuntimeEvent;
-  type ExistentialDeposit = ConstU64<1>;
-  type AccountStore = System;
-  type WeightInfo = ();
-  type MaxLocks = ();
-  type MaxReserves = ();
-  type ReserveIdentifier = [u8; 8];
+	type Balance = u64;
+	type DustRemoval = ();
+	type RuntimeEvent = RuntimeEvent;
+	type ExistentialDeposit = ConstU64<1>;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
 }
 
 pub struct AssetsCallbackHandle;
 impl AssetsCallback<AssetId, AccountId> for AssetsCallbackHandle {
-  fn created(_id: &AssetId, _owner: &AccountId) {
-    storage::set(b"asset_created", &().encode());
-  }
+	fn created(_id: &AssetId, _owner: &AccountId) {
+		storage::set(b"asset_created", &().encode());
+	}
 
-  fn destroyed(_id: &AssetId) {
-    storage::set(b"asset_destroyed", &().encode());
-  }
+	fn destroyed(_id: &AssetId) {
+		storage::set(b"asset_destroyed", &().encode());
+	}
 }
 
 parameter_types! {
@@ -128,34 +128,34 @@ parameter_types! {
 }
 
 impl Config for Test {
-  type RuntimeEvent = RuntimeEvent;
-  type Balance = u64;
-  type AssetId = u32;
-  type Rbac = RBAC;
-  type AssetIdParameter = u32;
-  type Currency = Balances;
-  type CreateOrigin = AsEnsureOriginWithArg<frame_system::EnsureSigned<u64>>;
-  type ForceOrigin = frame_system::EnsureRoot<u64>;
-  type AssetDeposit = ConstU64<1>;
-  type AssetAccountDeposit = ConstU64<10>;
-  type MetadataDepositBase = ConstU64<1>;
-  type MetadataDepositPerByte = ConstU64<1>;
-  type ApprovalDeposit = ConstU64<1>;
-  type StringLimit = ConstU32<50>;
-  type Freezer = TestFreezer;
-  type WeightInfo = ();
-  type CallbackHandle = AssetsCallbackHandle;
-  type Extra = ();
-  type RemoveItemsLimit = ConstU32<5>;
-  type MaxReserves = MaxReserves;
-  type ReserveIdentifier = u32;
+	type RuntimeEvent = RuntimeEvent;
+	type Balance = u64;
+	type AssetId = u32;
+	type Rbac = RBAC;
+	type AssetIdParameter = u32;
+	type Currency = Balances;
+	type CreateOrigin = AsEnsureOriginWithArg<frame_system::EnsureSigned<u64>>;
+	type ForceOrigin = frame_system::EnsureRoot<u64>;
+	type AssetDeposit = ConstU64<1>;
+	type AssetAccountDeposit = ConstU64<10>;
+	type MetadataDepositBase = ConstU64<1>;
+	type MetadataDepositPerByte = ConstU64<1>;
+	type ApprovalDeposit = ConstU64<1>;
+	type StringLimit = ConstU32<50>;
+	type Freezer = TestFreezer;
+	type WeightInfo = ();
+	type CallbackHandle = AssetsCallbackHandle;
+	type Extra = ();
+	type RemoveItemsLimit = ConstU32<5>;
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = u32;
 }
 
 use std::collections::HashMap;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Hook {
-  Died(u32, u64),
+	Died(u32, u64),
 }
 parameter_types! {
   static Frozen: HashMap<(u32, u64), u64> = Default::default();
@@ -164,61 +164,61 @@ parameter_types! {
 
 pub struct TestFreezer;
 impl FrozenBalance<u32, u64, u64> for TestFreezer {
-  fn frozen_balance(asset: u32, who: &u64) -> Option<u64> {
-    Frozen::get().get(&(asset, *who)).cloned()
-  }
+	fn frozen_balance(asset: u32, who: &u64) -> Option<u64> {
+		Frozen::get().get(&(asset, *who)).cloned()
+	}
 
-  fn died(asset: u32, who: &u64) {
-    Hooks::mutate(|v| v.push(Hook::Died(asset, *who)));
+	fn died(asset: u32, who: &u64) {
+		Hooks::mutate(|v| v.push(Hook::Died(asset, *who)));
 
-    // Sanity check: dead accounts have no balance.
-    assert!(Assets::balance(asset, *who).is_zero());
-  }
+		// Sanity check: dead accounts have no balance.
+		assert!(Assets::balance(asset, *who).is_zero());
+	}
 }
 
 pub(crate) fn set_frozen_balance(asset: u32, who: u64, amount: u64) {
-  Frozen::mutate(|v| {
-    v.insert((asset, who), amount);
-  });
+	Frozen::mutate(|v| {
+		v.insert((asset, who), amount);
+	});
 }
 
 pub(crate) fn clear_frozen_balance(asset: u32, who: u64) {
-  Frozen::mutate(|v| {
-    v.remove(&(asset, who));
-  });
+	Frozen::mutate(|v| {
+		v.remove(&(asset, who));
+	});
 }
 
 pub(crate) fn hooks() -> Vec<Hook> {
-  Hooks::get().clone()
+	Hooks::get().clone()
 }
 
 pub(crate) fn take_hooks() -> Vec<Hook> {
-  Hooks::take()
+	Hooks::take()
 }
 
 pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
-  let mut storage = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	let mut storage = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
 
-  let config: pallet_assets::GenesisConfig<Test> = pallet_assets::GenesisConfig {
-    assets: vec![
-      // id, owner, is_sufficient, min_balance
-      (999, 0, true, 1),
-    ],
-    metadata: vec![
-      // id, name, symbol, decimals
-      (999, "Token Name".into(), "TOKEN".into(), 10),
-    ],
-    accounts: vec![
-      // id, account_id, balance
-      (999, 1, 100),
-    ],
-  };
+	let config: pallet_assets::GenesisConfig<Test> = pallet_assets::GenesisConfig {
+		assets: vec![
+			// id, owner, is_sufficient, min_balance
+			(999, 0, true, 1),
+		],
+		metadata: vec![
+			// id, name, symbol, decimals
+			(999, "Token Name".into(), "TOKEN".into(), 10),
+		],
+		accounts: vec![
+			// id, account_id, balance
+			(999, 1, 100),
+		],
+	};
 
-  config.assimilate_storage(&mut storage).unwrap();
+	config.assimilate_storage(&mut storage).unwrap();
 
-  let mut ext: sp_io::TestExternalities = storage.into();
-  // Clear thread local vars for https://github.com/paritytech/substrate/issues/10479.
-  ext.execute_with(|| take_hooks());
-  ext.execute_with(|| System::set_block_number(1));
-  ext
+	let mut ext: sp_io::TestExternalities = storage.into();
+	// Clear thread local vars for https://github.com/paritytech/substrate/issues/10479.
+	ext.execute_with(|| take_hooks());
+	ext.execute_with(|| System::set_block_number(1));
+	ext
 }
