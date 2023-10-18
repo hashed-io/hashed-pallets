93d00b743481a58bfbdf5d2a8b9c02a87542be28
Tlalocman <desneruda@gmail.com>
2023-10-18 11:53:28 -0600

    🚀 chore(Cargo.toml): update dependencies versions for codec and scale-info crates to improve compatibility and stability
    🚀 chore(Cargo.toml): update dependencies versions for frame-benchmarking, frame-support, and frame-system crates to improve compatibility and stability
    
    🚀 chore(Cargo.toml): update dependencies versions for sp-core, sp-io, and sp-runtime crates to improve compatibility and stability
    
    🔨 refactor(lib.rs): remove unused imports and update documentation links
    
    🔨 refactor(lib.rs): remove unused storage item MyBytesVal and associated dispatchable insert_my_bytes
    
    🔨 refactor(lib.rs): remove unused migrations module
    
    🔨 refactor(lib.rs): remove unused tests module
    
    🔨 refactor(lib.rs): remove unused benchmarking module
    
    🔨 refactor(lib.rs): remove unused STORAGE_VERSION constant
    
    🔨 refactor(lib.rs): remove unused Pallet struct
    
    🔨 refactor(lib.rs): remove unused SomethingStored event parameter names
    
    🔨 refactor(lib.rs): remove unused MyBytes type
    
    🔨 refactor(lib.rs): remove unused Error variant parameter names
    
    🔨 refactor(lib.rs): remove unused cause_error dispatchable weight calculation
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable weight calculation
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable origin check
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable event emission
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable storage update
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable parameter names
    
    🔨 refactor(lib.rs): remove unused do_something dispatchable return type
    
    🔨 refactor(lib.rs): remove unused Config trait type WeightInfo
    
    🔨 refactor(lib.rs): remove unused Config trait type RuntimeEvent
    
    🔨 refactor(lib.rs): remove unused Config trait
    
    🔨 refactor(lib.rs): remove unused pallet module
    
    🔨 refactor(mock.rs): refactor mock runtime configuration to improve readability and remove unused code
    🔧 chore(mock.rs): update SS58Prefix to 42 for consistency with the network configuration

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -2,7 +2,7 @@
 
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
+/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
 #[cfg(test)]
@@ -13,53 +13,44 @@ mod tests;
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
-
-/// All migrations.
-pub mod migrations;
+pub mod weights;
+pub use weights::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-
+	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		/// Type representing the weight of this pallet
+		type WeightInfo: WeightInfo;
 	}
 
-	#[pallet::pallet]
-	#[pallet::storage_version(STORAGE_VERSION)]
-	
-	pub struct Pallet<T>(_);
-
 	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
+	// https://docs.substrate.io/main-docs/build/runtime-storage/
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
 	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
-	#[pallet::storage]
-	#[pallet::getter(fn my_bytes_val)]
-	pub type MyBytesVal<T> = StorageValue<_, MyBytes, ValueQuery>;
-
 	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
+	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		SomethingStored { something: u32, who: T::AccountId },
 	}
-
-	pub type MyBytes = BoundedVec<u8, ConstU32<16>>;
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
@@ -78,32 +69,32 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(Weight::from_parts(10_000,0) + T::DbWeight::get().writes(1))]
+		#[pallet::weight(T::WeightInfo::do_something())]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
+			// https://docs.substrate.io/main-docs/build/origins/
 			let who = ensure_signed(origin)?;
 
 			// Update storage.
 			<Something<T>>::put(something);
 
 			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
+			Self::deposit_event(Event::SomethingStored { something, who });
 			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
 
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
-		#[pallet::weight(Weight::from_parts(10_000,0) + T::DbWeight::get().reads_writes(1,1))]
+		#[pallet::weight(T::WeightInfo::cause_error())]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
 			let _who = ensure_signed(origin)?;
 
 			// Read a value from storage.
 			match <Something<T>>::get() {
 				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
+				None => return Err(Error::<T>::NoneValue.into()),
 				Some(old) => {
 					// Increment the value read from storage; will error in the event of overflow.
 					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
@@ -112,24 +103,6 @@ pub mod pallet {
 					Ok(())
 				},
 			}
-		}
-
-		#[pallet::call_index(2)]
-		#[pallet::weight(Weight::from_parts(10_000,0) + T::DbWeight::get().writes(1))]
-		pub fn insert_my_bytes(
-			origin: OriginFor<T>,
-			optional_bytes: Option<MyBytes>,
-		) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let _ = ensure_signed(origin)?;
-
-			// Update storage.
-			let s = optional_bytes.unwrap_or_default();
-			<MyBytesVal<T>>::put(s);
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
 		}
 	}
 }
